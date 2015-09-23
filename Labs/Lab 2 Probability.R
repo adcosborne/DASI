3b3b9f177911e13ff77f91cf599992a620ced6f0
mbh038 <mike038@btinternet.com>
2015-09-23 06:18:30 +0100

    Lab 2

---
+++ Labs/Lab 2 Probability.R
@@ -0,0 +1,39 @@
+## Date Analysis and Statistical Inference
+## Duke University / Coursera
+
+## Michael Hunt
+## September 2015
+
+## Lab 2:  Probability
+
+load(url("http://www.openintro.org/stat/data/kobe.RData"))
+head(kobe)
+
+kobe_streak <- calc_streak(kobe$basket)
+
+barplot(table(kobe_streak))
+median(kobe_streak)
+boxplot(kobe_streak)
+
+outcomes <- c("heads", "tails")
+sample(outcomes, size = 1, replace = TRUE)
+
+sim_fair_coin <- sample(outcomes, size = 100, replace = TRUE)
+
+sim_fair_coin
+table(sim_fair_coin)
+
+sim_unfair_coin <- sample(outcomes, size = 100, replace = TRUE, prob = c(0.2, 0.8))
+table(sim_unfair_coin)
+
+outcomes <- c("H", "M")
+sim_basket <- sample(outcomes, size = 133, replace = TRUE,prob=c(0.45,0.55))
+
+kobe$basket
+sim_basket
+
+sim_streak <- calc_streak(sim_basket)
+
+barplot(table(sim_streak))
+median(sim_streak)
+boxplot(sim_streak)
